6ba2d6dfb2702f96dd667e0525d23ea67f54d1ab
alt09 <andreslaratrevino@gmail.com>
2023-12-02 16:08:27 -0700

    create constants, create module,moduleIO,moduleIOSim,ModuleIOsparkMax files also advance a little in drive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,6 +15,7 @@ package frc.robot;
 
 import java.util.Optional;
 
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -54,7 +55,88 @@ public final class Constants {
       return DriverStation.getAlliance();
     }
 
-    public static final int CANConfigTimeout = 500;
-    public static final double loopPeriodSecs = 0.02;
-  }
+    public static final int CAN_CONFIG_TIMEOUT = 500;
+    public static final double LOOP_PERIOD_SECONDS = 0.02;
+
+    public static class DrivetrainConstants {
+
+      public static enum AbsoluteEncoder{
+        FRONT_LEFT(8),
+        FRONT_RIGTH(11),
+        BACK_LEFT(5),
+        BACK_RIGTH(2);
+  
+        public final int EncoderID;
+        AbsoluteEncoder(int ID){
+         EncoderID = ID;
+        }
+      }
+  
+      public static enum AbsoluteEncoderOffset{
+        FRONT_LEFT(-87.6269),
+        FRONT_RIGTH( -89.7363),
+        BACK_LEFT(-356.0449),
+        BACK_RIGTH(-177.6269);
+       
+        public final double offset;
+        AbsoluteEncoderOffset(double value){
+          offset = value;
+        }
+      }
+  
+      public static final double DRIVE_AFTER_ENCODER_REDUCTION = (50.0 / 14.0) * (17.0 / 27.0) * (45.0 / 15.0);
+  
+      public enum DriveMotor {
+        FRONT_LEFT(10),
+        FRONT_RIGTH(13),
+        BACK_LEFT(7),
+        BACK_RIGTH(4);
+  
+        public final int CAN_ID;
+  
+        
+  
+        DriveMotor(int value) {
+          CAN_ID = value;
+        }
+  
+      }
+  
+      public enum TurnMotor {
+        FRONT_LEFT(9),
+        FRONT_RIGTH(12),
+        BACK_LEFT(6),
+        BACK_RIGTH(3);
+  
+        public final int CAN_ID;
+  
+        TurnMotor(int value) {
+          CAN_ID = value;
+        }
+      }
+  
+      // Wheel Facts
+      public static double WHEEL_RADIUS_METERS = Units.inchesToMeters(2);
+  
+      // Under this to switch to coast when disabling
+      public static final double COAST_THERESHOULD_METERS_PER_SEC = 0.05;
+      // Under this speed for this length of time to switch to coast
+      public static final double  COAST_THERESHOULD_SEC = 6.0;
+      // Threshold to detect falls
+      public static final double LEDS_FALLEN_ANGLE_DEGREES = 60.0;
+  
+      public static final double MAX_LINEAR_SPEED = 4.5; //meters/sec 
+  
+      public static final double TRACK_WIDTH_X = Units.inchesToMeters(25); //21 from center of wheel to center of other wheel but 22 from encoder to encoder 
+      public static final double TRACK_WIDTH_Y = Units.inchesToMeters(25); //21 from center of wheel to center of other wheel but 22 from encoder to encoder
+  
+      private static final boolean IS_BRAKING_MODE = false;
+  
+      public static boolean IS_CHARACTERAZING = false;
+  
+    }
+    }
+    public static class OperatorConstants {
+      public static final int DRIVE_CONTROLLER = 0;
+    }
 } 
---
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -0,0 +1,102 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.drive;
+
+import frc.robot.Constants;
+import frc.robot.Constants.RobotConstants.DrivetrainConstants;
+import edu.wpi.first.math.util.Units;
+
+/** Add your docs here. */
+public class Drive {
+//TODO:change to english(it is in spanish)
+
+    //chassis
+
+    double[][] origin_pt = new double[4][2] ; // x,y coord of module point in vehicle coord sys
+    // module 0 is in +x,+y quadrant
+    // module 1 is in -x,+y quadrant
+    // module 2 is in -x,-y quadrant
+    // module 3 is in +x,-y quadrant
+
+    public double angle;//ungulo entre donde el robot mira y donde el robot esta derecho en el fild en radianes(gyro)(rad)
+    public double[] centerVel = new double[2];//Velocity in m/s from the center (in the direction where the robot is going)
+    public double angularVel;//veloity in degrees/s of how fast the robot spins 
+
+    //wheel
+    public double[] steeringAngle = new double[4];//es lo mismo que angle pero solo en las ruedas(in degrees)
+    public double[] wheelAngularVel = new double[4]; //velocidad de la rueda cuando gira no para avanzar si no para rotar en degrees/sec
+    
+    public double[] lastSteeringAngle = new double[4];//Last Steering Angle 
+    public double[] lastWheelAngularVel = new double[4]; //Last wheel angular velocity in degrees/sec
+
+    public double[] desiredSteeringAngle = new double[4];//es a donde tu quieres que valla la rueda in degrees
+    public double[] DesiredWeelAngularVel = new double[4];//es a cuantos degrees/sec tu quieres que valla la rueda 
+    
+    public double[] RecommendedSteeringAnglesDeg = new double[4] ;//angle in degree
+    public double[] RecommendedWheelAngularVel = new double[4] ;//degrees/sec
+
+    public void assign_veh_values_for_square_vehicle(double d, double wheel_d)
+    {
+        // set the wheel diameter
+        DrivetrainConstants.WHEEL_RADIUS_METERS = wheel_d ;
+        
+        // define the locations of the four swerve modules in the xv, yv coord sys
+        
+        this.origin_pt[0][0] =  d ;   // module 0 is in the +x,+y quadrant
+        this.origin_pt[0][1] =  d ;
+       
+        this.origin_pt[1][0] = -d ;   // module 1 is in the -x,+y quadrant
+        this.origin_pt[1][1] =  d ;
+        
+        this.origin_pt[2][0] = -d ;   // module 2 is in the -x,-y quadrant
+        this.origin_pt[2][1] = -d ;
+  
+        this.origin_pt[3][0] =  d ;   // module 3 is in the +x,-y quadrant
+        this.origin_pt[3][1] = -d ;
+    }
+    void initialize_steering_angles_and_wheel_speeds(double[] steering_ang_deg,
+    double[] wheel_speeds_deg_per_sec)
+{
+int i ;
+for (i=0 ; i<4 ; ++i)
+{
+this.steeringAngle[i] = steering_ang_deg[i] ;
+this.wheelAngularVel[i] = wheel_speeds_deg_per_sec[i] ;
+}
+}
+void calc_swerve_steering_and_speed()
+// This method uses the current values for:
+//        this.ang_rad,
+//        this.vel_pt0_m_per_sec
+//        this.ang_vel_deg_per_sec
+// and calculates and updates the values for:
+//        this.desired_steering_angles_deg  (a length 4 array of doubles)
+//        this.desired_wheel_angular_vel_deg_per_sec (a length 4 array of doubles)
+{
+int i ;
+double sv, cv ;
+
+cv = Math.cos(this.angle) ;
+sv = Math.sin(this.angle) ;
+
+// calculate the vehicle velocity in the vehicle coord sys
+double[] vO_in_veh = new double [2] ;
+vO_in_veh[0] =  cv * this.centerVel[0] + sv * this.centerVel[1] ;
+vO_in_veh[1] = -sv * this.centerVel[0] + cv * this.centerVel[1] ;
+
+double[] v_i = new double[2] ;
+double speed ;
+
+for (i=0 ; i<4 ; ++i)
+{
+    v_i[0] = vO_in_veh[0] - this.angularVel*Units.degreesToRadians( this.origin_pt[i][1]) ;
+    v_i[1] = vO_in_veh[1] + this.angularVel*Units.degreesToRadians( this.origin_pt[i][0]) ;
+
+    speed = Math.sqrt(v_i[0]*v_i[0] + v_i[1]*v_i[1]) ;
+    this.lastSteeringAngle[i] = Units.degreesToRadians(Math.atan2(v_i[1], v_i[0]))  ;
+    this.lastWheelAngularVel[i] = Units.degreesToRadians( speed) / ( DrivetrainConstants.WHEEL_RADIUS_METERS) ;       
+}    
+}
+}
